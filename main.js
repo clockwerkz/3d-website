d4d0c60a4a376065909e298d5625211a040a24b5
Carlos <clockwerkz@gmail.com>
2021-06-22 23:54:09 -0700

    adds basic torus, lights, and orbit controls

--- main.js
+++ main.js
@@ -1,6 +1,52 @@
-import './style.css'
+import './style.css';
 
-document.querySelector('#app').innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+
+//always need 3 objects - a scene, a camera, and a renderer
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGL1Renderer({
+  canvas : document.getElementById("bg"),
+})
+
+renderer.setPixelRatio( window.devicePixelRatio );
+renderer.setSize( window.innerWidth, window.innerHeight );
+camera.position.setZ(30);
+
+const geometry = new THREE.TorusGeometry( 10, 3, 16, 100 );
+const material = new THREE.MeshStandardMaterial( {
+  color: 0xFF6347,
+} );
+
+const pointLight = new THREE.PointLight(0xFFFFFF);
+const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+pointLight.position.set(5,5,5);
+scene.add(pointLight, ambientLight);
+
+const torus = new THREE.Mesh( geometry, material );
+scene.add(torus);
+
+const lightHelper = new THREE.PointLightHelper(pointLight);
+const gridHelper = new THREE.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+  renderer.render( scene, camera );
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+
+  controls.update();
+
+  requestAnimationFrame( animate );
+}
+
+animate();
